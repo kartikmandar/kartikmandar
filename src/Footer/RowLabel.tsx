f49b49d2b7179b62b1d50e209c8b7635d4166316
Kartik Mandar <kartik4321mandar@gmail.com>
2025-04-27 16:29:46 +0530

    fix errors

--- src/Footer/RowLabel.tsx
+++ src/Footer/RowLabel.tsx
@@ -1,9 +1,15 @@
 'use client'
-import { Header } from '@/payload-types'
 import { RowLabelProps, useRowLabel } from '@payloadcms/ui'
 
+interface NavItem {
+  link?: {
+    label?: string;
+    url?: string;
+  };
+}
+
 export const RowLabel: React.FC<RowLabelProps> = () => {
-  const data = useRowLabel<NonNullable<Header['navItems']>[number]>()
+  const data = useRowLabel<NavItem>()
 
   const label = data?.data?.link?.label
     ? `Nav item ${data.rowNumber !== undefined ? data.rowNumber + 1 : ''}: ${data?.data?.link?.label}`
