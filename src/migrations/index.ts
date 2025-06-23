7a9be6aeffcb7cd60f0c082305e40b279b7f1bc0
Kartik Mandar <kartik4321mandar@gmail.com>
2025-06-23 21:27:42 +0530

    Fix migration error: add safe enum checks and TypeScript error

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,5 +1,5 @@
 import * as migration_20250621_140000_add_missing_block_tables from './20250621_140000_add_missing_block_tables';
-import * as migration_20250622_112307_fix_pages_rels_table from './20250622_112307_fix_pages_rels_table';
+import * as migration_20250622_112307_fix_pages_rels_table_safe from './20250622_112307_fix_pages_rels_table_safe';
 
 export const migrations = [
   {
@@ -8,8 +8,8 @@ export const migrations = [
     name: '20250621_140000_add_missing_block_tables',
   },
   {
-    up: migration_20250622_112307_fix_pages_rels_table.up,
-    down: migration_20250622_112307_fix_pages_rels_table.down,
-    name: '20250622_112307_fix_pages_rels_table'
+    up: migration_20250622_112307_fix_pages_rels_table_safe.up,
+    down: migration_20250622_112307_fix_pages_rels_table_safe.down,
+    name: '20250622_112307_fix_pages_rels_table_safe'
   },
 ];
