24571eeb3fc7f1ee84be7c4a11adb1d7a3c41164
Kartik Mandar <kartik4321mandar@gmail.com>
2025-06-24 22:50:43 +0530

    feat: add Vercel Analytics for visitor and page view tracking

--- src/app/(frontend)/layout.tsx
+++ src/app/(frontend)/layout.tsx
@@ -4,6 +4,7 @@ import { cn } from '@/utilities/ui'
 import { GeistMono } from 'geist/font/mono'
 import { GeistSans } from 'geist/font/sans'
 import React from 'react'
+import { Analytics } from '@vercel/analytics/next'
 
 import { AdminBar } from '@/components/AdminBar'
 import { ExternalLinkHandler } from '@/components/ExternalLinkHandler'
@@ -45,6 +46,7 @@ export default async function RootLayout({ children }: { children: React.ReactNo
           {children}
           <Footer />
           <FloatingBottomNav navItems={navItems} />
+          <Analytics />
         </Providers>
       </body>
     </html>
